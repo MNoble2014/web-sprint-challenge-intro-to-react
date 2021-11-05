f3e8358f4f9bb4ab1385ddf7390ca35eeecf3669
MNoble2014 <michealsimpson2013@gmail.com>
2021-11-05 12:37:09 -0400

    App.js completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import { Character } from './components/Character';
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/films')
+    .then(res => {
+      setData(res.data.results)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+  console.log(data) 
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +27,13 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className='container'>
+        <div>
+          {data.map(item => {
+            return <Character key={item.id} data={item} />
+          })}
+        </div>
+      </div>
     </div>
   );
 }
