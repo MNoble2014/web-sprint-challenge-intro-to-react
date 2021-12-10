145f4523fbe853e1c1038828aee17e2ce8c73e49
MNoble2014 <michealsimpson2013@gmail.com>
2021-12-10 12:05:10 -0500

    character.js updated

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,105 +1,29 @@
 // Write your Character component here
-import React from "react"; //TODO: doubt i'll need to add a useState or effect here
+import React from 'react';
+import styled from 'styled-components';
 
-import Films from "./Films/Films";
-
-//styling
-import styled, {keyframes} from "styled-components";
-
-const StyledDiv = styled.div`
-    display: flex;
-    border: 1px solid black;
-    flex-flow: row wrap;
-    justify-self: center;
-    margin: 2%;
-    height: auto;
-    //* width: fill; //(not necessary for MVP)
-    padding: 0% 2%;
-    transition: 0.2s ease-in-out;
-    //? ---- HOVERS
-    &:hover{
-        transition: all 0.2s ease-in-out;
-        background-color: ${pr => pr.theme.transBColor};
-        h1{
-            color:  ${pr => pr.theme.transTColor};
-            text-shadow: 2px 2px ${pr => pr.theme.textColor};
-        }
-        button{
-            background-color:  ${pr => pr.theme.textColor};
-            color:  ${pr => pr.theme.backColor};
-        }
-    };
-    
-    div {
-        display: flex;
-        width: 100%;
-        justify-content: space-between;
-        color: ${pr => pr.theme.textColor};
-        text-shadow: 2px 2px ${pr => pr.theme.backColor};
-        
-        //? ----- for the Name initial
-        h1{
-            display: flex;
-            //*  align-self: left;  //(not necessary for MVP)
-            font: 30pt News Gothic, sans-script;
-            margin: 0;
-        }
-        //? ------ for the button at initial
-        button{
-            align-self: flex-end;
-            height: fit-content;
-            background-color: ${pr => pr.theme.backColor};
-            color: ${pr => pr.theme.textColor};
-            //?additional HOVERS only while on button
-            :hover{
-                transition: 0.5s ease-in-out;
-                background-color:  ${pr => pr.theme.backColor};
-                color:  ${pr => pr.theme.textColor};                
-            }
-        }
-    }
-    .films{
-        display: flex;
-        //  border: 3px dotted red;
-        width: 100%;
-    }
+const StarStyle = styled.div`
+display:flex;
+justify-content:center;
+margin:10% 15% 0;
+background-color:coral;
+border: solid black 5px;
+border-radius:10%;
+padding:5%;
 `
 
-export default function Character(props){
-    const { name, 
-            birthyear, 
-            films, 
-            actionFilms, 
-            charId, 
-            actionMovie, 
-            filmData,
-            filmId } = props;
 
-     console.log(filmId)
-    return(
-        <>
-        <StyledDiv>
-            <div className='HERE'>
-                <h1>{name}</h1>
-                <button onClick={() => actionFilms(films)} >{birthyear}</button>  {/* *****TODO onClick={(() => onClick)} to show films **** */}
-            </div>
-
-            <div className="films"> 
-            { films === charId ?  films.map((el, index) => {
-                    return (
-                        <Films 
-                            key={index}
-                            films={el}
-                            actionMovie={actionMovie}
-                            filmData={filmData}
-                            filmId={filmId}
-                        /> 
-                        ) 
-                    }) 
-            : null }
-                </div>
-        </StyledDiv>
-        </>
-    );
-
+    
+const Character = props => {
+    const {info} =props
+    return (
+        <StarStyle>
+            
+            <p><b>{info.name}</b> was born in {info.birth_year}. 
+            They are {info.height}km tall.</p>
+        </StarStyle>
+    )
 }
+
+
+export default Character;
