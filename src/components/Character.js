d4013c06c773e8f988f3d42c8ecb892f8c553bd3
MNoble2014 <michealsimpson2013@gmail.com>
2021-12-10 11:53:41 -0500

    character.js updated

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,24 +1,23 @@
 // Write your Character component here
-import React from 'react';
 import styled from 'styled-components';
+import CharacterCard from './CharacterCard.js'
+import theme from '../theme/index.js';
 
-const StarStyle = styled.div`
+const StyledCharacter = styled.div`
 display: flex;
-justify-content: center;
-margin: 10% 15% 0;
-background-color: coral;
-border: solid black 5px;
-border-radius: 10%10%;
-padding: 5%
+flex-direction:column;
+width:60%;
+align-items: center;
+color: ${theme.secondaryColor};
+border-radius: 10%;
 `
+export default function Character({items}) {
 
-const Character = props => {
-    const {info} = props
-    return (
-        <StarStyle>
-            <p>{info.name} was born in {info.birth_year}.<b>
-            They are {info.height} km tall.</b></p>
-        </StarStyle>
-    )
-}
-export default Character;
+return(
+    <StyledCharacter>
+     {items.map((item)=>(
+        <CharacterCard item = {item} key={item.id}/>
+     ))
+     }
+    </StyledCharacter>
+)}
